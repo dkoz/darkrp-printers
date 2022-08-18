cf0975788267780e3e0424b0f22e82c50491fc0f
dkoz <drunkenkoz@gmail.com>
2022-08-18 11:07:27 -0400

    Initial Commit of Printers

---
+++ lua/entities/printer_cooler/shared.lua
@@ -0,0 +1,9 @@
+ENT.Type = "anim"
+ENT.Base = "base_gmodentity"
+
+ENT.PrintName = "Printer Cooler"
+ENT.Author = "KoZ"
+ENT.Category = "Oasis RP"
+
+ENT.Spawnable = true
+ENT.AdminOnly = true
---
+++ lua/entities/printer_storing/shared.lua
@@ -0,0 +1,12 @@
+ENT.Type = "anim"
+ENT.Base = "base_gmodentity"
+ENT.PrintName = "Storing Printer"
+ENT.Author = "KoZ"
+ENT.Category = "Oasis RP"
+ENT.Spawnable = true
+ENT.AdminOnly = true
+
+function ENT:SetupDataTables()
+	self:NetworkVar( "Int", 0, "price" )
+	self:NetworkVar( "Entity", 1, "owning_ent" )
+end
